f7f90404ebcfc8f11b7f65dace9de14e690a9e66
Yash Londhe <yashlondhe9151@gmail.com>
2023-03-19 23:21:23 +0530

    added half work

---
+++ services/user.service.go
@@ -0,0 +1,11 @@
+package services
+
+import "example.com/yash-apis/models"
+
+type UserService interfeace {
+	CreateUser(*models.User) error
+	GetUser(*string) (*models.User, error)
+	GetAll() ([] *models.User, error)
+	UpdateUser(*models.User) error
+	DeleteUser(*string) error
+}
