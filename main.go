ad6e1bdded8beb54f8ee9ea347fd95616300b750
Yash Londhe <yashlondhe9151@gmail.com>
2023-03-22 23:50:16 +0530

    Did some changes

--- main.go
+++ main.go
@@ -1,20 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"log"
 
-var{
-	server *gin.Engine
+	"example.com/yash-apis/controllers"
+	"example.com/yash-apis/services"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
 
-	userservice services.UserService
-	usercontroller controllers.UserController
-	ctx context.Context
-	usercollection *mongo.Collection
+
+var (
+	server      *gin.Engine
+	us          services.UserService
+	uc          controllers.UserController
+	ctx         context.Context
+	userc       *mongo.Collection
 	mongoclient *mongo.Client
-	err error
+	err         error
+)
 
-}
-func init(){
+func init() {
 	ctx = context.TODO()
+
 	mongoconn := options.Client().ApplyURI("mongodb://localhost:27017")
 	mongoclient, err = mongo.Connect(ctx, mongoconn)
 	if err != nil {
@@ -24,5 +36,21 @@ func init(){
 	if err != nil {
 		log.Fatal("error while trying to ping mongo", err)
 	}
+
+	fmt.Println("mongo connection established")
+
+	userc = mongoclient.Database("userdb").Collection("users")
+	us = services.NewUserService(userc, ctx)
+	uc = controllers.New(us)
+	server = gin.Default()
 }
-func main() {}
+
+func main() {
+	defer mongoclient.Disconnect(ctx)
+
+	basepath := server.Group("/v1")
+	uc.RegisterUserRoutes(basepath)
+
+	log.Fatal(server.Run(":9090"))
+
+}
