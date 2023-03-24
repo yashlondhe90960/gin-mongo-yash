172c787eea819cddcf23dc5daf863551b6cc06c7
Yash Londhe <99034957+yashlondhe90960@users.noreply.github.com>
2023-03-24 23:47:37 +0530

    Update user.service.go

--- services/user.service.go
+++ services/user.service.go
@@ -5,7 +5,5 @@ import "example.com/yash-apis/models"
 type UserService interfeace {
 	CreateUser(*models.User) error
 	GetUser(*string) (*models.User, error)
-	GetAll() ([] *models.User, error)
-	UpdateUser(*models.User) error
-	DeleteUser(*string) error
+    	
 }
